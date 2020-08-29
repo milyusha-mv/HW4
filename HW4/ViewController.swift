a3bc3824b325f0eb8ec1e4cd2f0773931547589d
Mikhail M <milyusha-mv@ural.rt.ru>
2020-08-29 22:00:08 +0500

    Initial Commit

---
+++ HW4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW4
+//
+//  Created by Михаил Милюша on 29.08.2020.
+//  Copyright © 2020 Mikhail Miliusha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
